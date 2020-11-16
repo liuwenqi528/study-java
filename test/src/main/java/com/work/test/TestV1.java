7dd768cb24ebdb25a33648d5786e9df4399c6008
liuwenqi528 <410587745@qq.com>
2020-11-16 10:25:59 +0800

    v1

---
+++ test/src/main/java/com/work/test/TestV1.java
@@ -0,0 +1,15 @@
+package com.work.test;
+
+/**
+ * ClassName: TestV1 <br/>
+ * Description: <br/>
+ * date: 2020-11-16 10:24<br/>
+ *
+ * @author lwq<br />
+ * @since JDK 1.8
+ */
+public class TestV1 {
+    public static void main(String[] args) {
+
+    }
+}
